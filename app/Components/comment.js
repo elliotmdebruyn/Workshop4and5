d27d97f56bac8897d1680529b4d5016f9f1e8163
Elliot DeBruyn <Elliot DeBruyn>
2017-04-06 16:30:14 -0400

    fb8

--- app/Components/comment.js
+++ app/Components/comment.js
@@ -2,7 +2,58 @@ import React from 'react';
 import unixTimeToString from '../util';
 import {Link} from 'react-router';
 
+import {unlikeComment} from '../server.js';
+import {likeComment} from '../server.js';
+
 export default class Comment extends React.Component {
+  constructor(props) {
+      super(props);
+      this.state = props;
+    }
+
+    handleLikeClick(clickEvent) {
+      // Stop the event from propagating up the DOM tree, since we handle it here.
+      // Also prevents the link click from causing the page to scroll to the top.
+      clickEvent.preventDefault();
+      // 0 represents the 'main mouse button' -- typically a left click
+      // https://developer.mozilla.org/en-US/docs/Web/API/MouseEvent/button
+      if (clickEvent.button === 0) {
+        // Callback function for both the like and unlike cases.
+        var callbackFunction = (updatedLikeCounter) => {
+          // setState will overwrite the 'likeCounter' field on the current
+          // state, and will keep the other fields in-tact.
+          // This is called a shallow merge:
+          // https://facebook.github.io/react/docs/component-api.html#setstate
+          this.setState({likeCounter: updatedLikeCounter});
+        };
+
+        if (this.didUserLike()) {
+          // User clicked 'unlike' button.
+          unlikeComment(this.state.attr, 4, this.state.commentInd, callbackFunction);
+        } else {
+          // User clicked 'like' button.
+          likeComment(this.state.attr, 4, this.state.commentInd, callbackFunction);
+        }
+      }
+    }
+
+    /**
+     * Returns 'true' if the user liked the item.
+     * Returns 'false' if the user has not liked the item.
+     */
+    didUserLike() {
+      var likeCounter = this.state.likeCounter;
+      var liked = false;
+      // Look for a likeCounter entry with userId 4 -- which is the
+      // current user.
+      for (var i = 0; i < likeCounter.length; i++) {
+        if (likeCounter[i]._id === 4) {
+          liked = true;
+          break;
+        }
+      }
+      return liked;
+    }
   render() {
     return (
       <div>
--- app/server.js
+++ app/server.js
@@ -1,6 +1,16 @@
 import {readDocument, writeDocument, addDocument} from './database.js';
 
 /**
+ * Emulates how a REST call is *asynchronous* -- it calls your function back
+ * some time in the future with data.
+ */
+function emulateServerReturn(data, cb) {
+  setTimeout(() => {
+    cb(data);
+  }, 4);
+}
+
+/**
  * Given a feed item ID, returns a FeedItem object with references resolved.
  * Internal to the server, since it's synchronous.
  */
@@ -56,30 +66,6 @@ export function unlikeFeedItem(feedItemId, userId, cb) {
   emulateServerReturn(feedItem.likeCounter.map((userId) => readDocument('users', userId)), cb);
 }
 
-/**
- * Emulates a REST call to get the feed data for a particular user.
- * @param user The ID of the user whose feed we are requesting.
- * @param cb A Function object, which we will invoke when the Feed's data is available.
- */
-export function getFeedData(user, cb) {
-  // Get the User object with the id "user".
-  var userData = readDocument('users', user);
-  // Get the Feed object for the user.
-  var feedData = readDocument('feeds', userData.feed);
-  // Map the Feed's FeedItem references to actual FeedItem objects.
-  // Note: While map takes a callback function as an argument, it is
-  // synchronous, not asynchronous. It calls the callback immediately.
-  feedData.contents = feedData.contents.map(getFeedItemSync);
-  // Return FeedData with resolved references.
-  // emulateServerReturn will emulate an asynchronous server operation, which
-  // invokes (calls) the "cb" function some time in the future.
-  emulateServerReturn(feedData, cb);
-}
-
-/**
- * Adds a new comment to the database on the given feed item.
- * Returns the updated FeedItem object.
- */
 export function postComment(feedItemId, author, contents, cb) {
   // Since a CommentThread is embedded in a FeedItem object,
   // we don't have to resolve it. Read the document,
@@ -97,9 +83,6 @@ export function postComment(feedItemId, author, contents, cb) {
   emulateServerReturn(getFeedItemSync(feedItemId), cb);
 }
 
-/**
- * Adds a new status update to the database.
- */
 export function postStatusUpdate(user, location, contents, cb) {
   // If we were implementing this for real on an actual server, we would check
   // that the user ID is correct & matches the authenticated user. But since
@@ -137,8 +120,22 @@ export function postStatusUpdate(user, location, contents, cb) {
   emulateServerReturn(newStatusUpdate, cb);
 }
 
-function emulateServerReturn(data, cb) {
-  setTimeout(() => {
-    cb(data);
-  }, 4);
+/**
+ * Emulates a REST call to get the feed data for a particular user.
+ * @param user The ID of the user whose feed we are requesting.
+ * @param cb A Function object, which we will invoke when the Feed's data is available.
+ */
+export function getFeedData(user, cb) {
+  // Get the User object with the id "user".
+  var userData = readDocument('users', user);
+  // Get the Feed object for the user.
+  var feedData = readDocument('feeds', userData.feed);
+  // Map the Feed's FeedItem references to actual FeedItem objects.
+  // Note: While map takes a callback function as an argument, it is
+  // synchronous, not asynchronous. It calls the callback immediately.
+  feedData.contents = feedData.contents.map(getFeedItemSync);
+  // Return FeedData with resolved references.
+  // emulateServerReturn will emulate an asynchronous server operation, which
+  // invokes (calls) the "cb" function some time in the future.
+  emulateServerReturn(feedData, cb);
 }
